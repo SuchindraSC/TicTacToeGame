6415ba9314b125122b74e3ad6de1cd83be74db3c
SuchindraSC <suchindrachitnis@gmail.com>
2021-09-01 10:37:22 +0530

    TicTacToe

--- TicTacToeBoard/TicTacToeBoard/TicToeGame.cs
+++ TicTacToeBoard/TicTacToeBoard/TicToeGame.cs
@@ -6,9 +6,28 @@ namespace TicTacToeBoard
 {
     public  class TicToeGame
     {
+        public static string computer;
         public static void tictactoeUC1()
         {
-            char[] board = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            char[] board = new char[10];
+            for (int i = 0; i <= board.Length; i ++)
+            {
+                board[i] = ' ';
+            }
+        }
+        public static void tictactoeUC2()
+        {
+            
+            Console.WriteLine("Select 'X' or 'O': ");
+            string player = Console.ReadLine();
+            if(player == "X")
+            {
+                computer = "O";
+            }
+            else
+            {
+                computer = "X";
+            }
         }
     }
 }

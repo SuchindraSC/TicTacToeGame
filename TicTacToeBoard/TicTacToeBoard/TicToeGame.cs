080933bea97d9de00041f8b2f29ddbfe375119a5
SuchindraSC <85979972+SuchindraSC@users.noreply.github.com>
2021-09-01 10:00:37 +0530

    Update TicToeGame.cs

--- TicTacToeBoard/TicTacToeBoard/TicToeGame.cs
+++ TicTacToeBoard/TicTacToeBoard/TicToeGame.cs
@@ -6,6 +6,9 @@ namespace TicTacToeBoard
 {
     public static class TicToeGame
     {
-        static char[] board = { '0','1','2','3','4','5','6','7','8','9'};
+        static void Main(string[] args)
+        {
+            TicToeGame.tictactoeUC1();
+        }
     }
 }

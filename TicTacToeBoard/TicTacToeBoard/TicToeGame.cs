d5728406fcea12f4463560dec8981d58d0e09640
SuchindraSC <suchindrachitnis@gmail.com>
2021-09-01 09:44:39 +0530

    TicTacToe Game

---
+++ TicTacToeBoard/TicTacToeBoard/TicToeGame.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeBoard
+{
+    public static class TicToeGame
+    {
+        static char[] board = { '0','1','2','3','4','5','6','7','8','9'};
+    }
+}
